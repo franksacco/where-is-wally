ad4097a4aa62537be71ffc609c6caca280559e66
Francesco Saccani <saccani.francesco@gmail.com>
2020-04-04 19:38:47 +0200

    Android application skeleton

---
+++ app/src/main/java/it/unipr/advmobdev/whereiswally/MainActivity.java
@@ -0,0 +1,14 @@
+package it.unipr.advmobdev.whereiswally;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
